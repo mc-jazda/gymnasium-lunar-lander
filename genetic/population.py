ce5660d384dd8bc3897caa8ad5263e092730fe94
Jakub Gazda <kgazda53@gmail.com>
2024-05-16 14:38:57 +0200

    normalize fitness score before roulette selection

--- genetic/population.py
+++ genetic/population.py
@@ -32,7 +32,7 @@ class Population:
             bot.sum_reward = 0
     
     def get_best_fitness(self):
-        """Returns greatest fitness value in current generation"""
+        """Returns greatest fitness value in the current generation"""
         best_fitness = -10000000.0
 
         for bot in self.bots:
@@ -41,6 +41,17 @@ class Population:
                 best_fitness = fitness
 
         return best_fitness
+
+    def get_worst_fitness(self):
+        """Returns smallest fitness value in the current generation"""
+        worst_fitness = 1000000.0
+
+        for bot in self.bots:
+            fitness = bot.fitness()
+            if fitness < worst_fitness:
+                worst_fitness = fitness
+        
+        return worst_fitness
     
     def get_best_bot(self):
         """Returns bot with greatest fitness value in current generation"""
@@ -55,6 +66,38 @@ class Population:
         
         return best_bot
 
+    def normalize_fitness(self):
+        """Normalizes fitness scores in order to conduct roulette selection."""
+        worst_fitness = self.get_worst_fitness()
+        
+        if worst_fitness < 0:
+            for bot in self.bots:
+                bot.reward(1 - worst_fitness)
+
+    def get_total_score(self):
+        """Returns sum of fitness scores of all bots in the current generation."""
+        total_score = 0
+        for bot in self.bots:
+            total_score += bot.fitness()   
+
+        return total_score
+    
+    def roulette_selection(self):
+        """Roulette selection algorithm. Part of genetic algorithm.
+        Returns:
+            selected bot
+        """
+        self.normalize_fitness()
+        total_score = self.get_total_score()
+
+        relative_score = [bot.fitness() / total_score for bot in self.bots]
+        cumulative_probability = [sum(relative_score[:i+1]) for i in range(len(relative_score))]
+
+        rand = random.random()
+        for i, cp in enumerate(cumulative_probability):
+            if rand <= cp:
+                return self.bots[i]
+
     def crossover(self):
         """Creates new bots out of crossover of two other bots. Part of genetic algorithm."""
         # choose top num_surviving bots
@@ -65,8 +108,6 @@ class Population:
         # generate new bots
         for i in range(self.num_new):
             # choose parents and cross point
-            #first_parent = random.choice(self.bots).matrix.flatten() #tu rewolwer 
-            #second_parent = random.choice(self.bots).matrix.flatten() # tu rewolwer
             first_parent = self.roulette_selection().matrix.flatten()
             second_parent = self.roulette_selection().matrix.flatten()
             cross_point = random.randint(1, 32)
@@ -90,26 +131,3 @@ class Population:
             
             matrix = matrix.reshape((8,4))
             bot.matrix = matrix
-    
-    def get_total_score(self):
-        total_score = 0
-        for bot in self.bots:
-            total_score += bot.fitness()    
-        return total_score
-    
-    def roulette_selection(self):
-        total_score = self.get_total_score()
-        relative_score = [bot.fitness() / total_score for bot in self.bots]
-        cumulative_probability = [sum(relative_score[:i+1]) for i in range(len(relative_score))]
-        rand = random.random()
-        for i, cp in enumerate(cumulative_probability):
-            if rand <= cp:
-                return self.bots[i]
-
-        
-if __name__ == '__main__':
-    pop = Population(num_surviving=1, num_new=1)
-    print(pop)
-    print('\n-------------------------------------------------------')
-    pop.mutation(0.5)
-    print(pop)
